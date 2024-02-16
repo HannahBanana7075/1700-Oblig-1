9e337f828101cb96f1131df84043b4819687d8f3
HannahBanana7075 <143513064+HannahBanana7075@users.noreply.github.com>
2024-02-17 00:35:19 +0100

    function showTickets, div, style

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let tickets = [];
 
 function addTicket(ticket){
-    tickets.push(ticket)
+    tickets.push(ticket);
+    showTicketList();
 }
 
 function validation(){
+    console.log("I have been called");
     let amount = document.getElementById("amount").value;
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     let phoneNumber = document.getElementById("phoneNumber").value;
     let email = document.getElementById("email").value;
     //do validation here
-
 
     let ticket = {
         amount: amount,
@@ -22,4 +23,25 @@ function validation(){
     }
 
     addTicket(ticket)
+}
+
+function showTicketList(){
+    let myHTML = [];
+    let headers = `
+    <table>
+    <tr>
+    <th>Amount</th>
+    <th>First name</th>
+    <th>Last name</th>
+    <th>Phone number</th>
+    <th>E-Mail</th>
+    </tr>
+    `;
+    myHTML.push(headers);
+
+
+
+
+    let result = myHTML.join();
+    document.getElementById("allTickets").innerHTML= result;
 }
